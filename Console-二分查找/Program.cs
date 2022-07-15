01f86942e5e1653ed38e0b563a9cfaa1b9d759e8
JWJUN233233 <JWJUN233233@users.noreply.github.com>
2022-07-15 15:44:50 +0800

    添加项目文件。

---
+++ Console-二分查找/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            List<int> ints = new();
+            for (int i = 0; i < 100; i++)
+            {
+                ints.Add(random.Next(0,1000));
+            }
+            ints.Sort();
+            foreach(int tmp in ints)
+            {
+                System.Console.WriteLine(tmp);
+            }
+            System.Console.WriteLine("\n\n\n" + BinarySearch(ints, int.Parse(System.Console.ReadLine())).ToString());
+        }
+        static int? BinarySearch(List<int> List,int value)
+        {
+            int left = 0;
+            int right = List.Count - 1;
+            int mid = List.Count / 2;
+            while (left <= right)
+            {
+                if(List[mid] == value)
+                {
+                    return mid;
+                }
+                else if (List[mid] > value)
+                {
+                    right = mid - 1;
+                }
+                else if (List[mid] < value)
+                {
+                    left = mid + 1;
+                }
+                mid = (right + left) / 2;
+            }
+            return null ;
+        }
+    }
+}
---
+++ Console/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Console
+{
+    class Program
+    {
+        static string Movement;
+        static void Main(string[] args)
+        {
+            Hanoi(10);
+            //File.WriteAllText("Out.txt",Movement);
+        }
+        static void Hanoi(int n,string a = "a",string b = "b",string c = "c")
+        {
+            if (n > 0)
+            {
+                Hanoi(n - 1, a, c, b);
+                System.Console.WriteLine(a + "  To   " + c);
+                //Movement += a + "  To   " + c + "\n";
+                Hanoi(n - 1, b, a, c);
+            }
+        }
+    }
+}
